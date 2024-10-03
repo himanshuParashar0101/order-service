2ac583d379fc5e7d7c0a2615c22cd1676d01e7b5
Himanshu Parashar <himanshuparashar0101@gmail.com>
2024-10-03 15:43:39 +0530

    changing port to 3003

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003; //changing port
 
 app.listen(PORT, () => {
   console.log(`✅ Order service running on port ${PORT}`);
